35b91341bd49c6943712ccd2477959f9dd2ba5d2
FrandyGuerra <frandyguerra2012@gmail.com>
2021-09-23 00:45:50 +0000

    este es el code de generator de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("p").innerHTML = `${arregloAleatorio(
+    who
+  )} ${arregloAleatorio(action)} ${arregloAleatorio(what)} ${arregloAleatorio(
+    when
+  )}`;
 };
+
+function arregloAleatorio(arr) {
+  let posicionArreglo = Math.floor(Math.random() * arr.length);
+  return arr[posicionArreglo];
+}
